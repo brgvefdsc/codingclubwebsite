3beb9e9518c493eb34112c4fdb129714a669829c
brgvefdsc <91153971+brgvefdsc@users.noreply.github.com>
2023-05-31 13:08:25 -0700

    Delete code.js

--- code.js
+++
@@ -1,8 +0,0 @@
-var i = 0 
-function myFunction() {
-  i = i + 1
-  if (i==10){
-//    document.getElementById("demo").innerHTML = "Hello World";
-    document.getElementById("bod").style.fontFamily = "Comic Sans MS";
-	}
-}
